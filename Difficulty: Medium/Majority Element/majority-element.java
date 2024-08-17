1f2413322240143ec911018a4965be3caa35af0c
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-08-17 22:17:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int majority = a.length / 2;
+
+        for(int n : a){
+            map.put(n, 1 + map.getOrDefault(n, 0));
+            if(map.get(n) > majority){
+                return n;
+            }
+        }
+        
+        return -1; 
+    }
+}
