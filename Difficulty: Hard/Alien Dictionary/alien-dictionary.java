6cbab41c797edb47d6b6b2ea19f9b90113729efa
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-08-22 19:55:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.java
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = Integer.parseInt(sc.next());
+        while (t-- > 0) {
+            int n = Integer.parseInt(sc.next());
+            int k = Integer.parseInt(sc.next());
+
+            String[] words = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                words[i] = sc.next();
+            }
+
+            Solution ob = new Solution();
+            //  System.out.println(T.findOrder(words,k));
+            String order = ob.findOrder(words, n, k);
+            if (order.length() == 0) {
+                System.out.println(0);
+                continue;
+            }
+            if (order.length() != k) {
+                System.out.println("INCOMPLETE");
+                return;
+            }
+            String temp[] = new String[n];
+            for (int i = 0; i < n; i++) temp[i] = words[i];
+
+            Arrays.sort(temp, new Comparator<String>() {
+                @Override
+                public int compare(String a, String b) {
+                    int index1 = 0;
+                    int index2 = 0;
+                    for (int i = 0;
+                         i < Math.min(a.length(), b.length()) && index1 == index2;
+                         i++) {
+                        index1 = order.indexOf(a.charAt(i));
+                        index2 = order.indexOf(b.charAt(i));
+                    }
+
+                    if (index1 == index2 && a.length() != b.length()) {
+                        if (a.length() < b.length())
+                            return -1;
+                        else
+                            return 1;
+                    }
+
+                    if (index1 < index2)
+                        return -1;
+                    else
+                        return 1;
+                }
+            });
+
+            int flag = 1;
+            for (int i = 0; i < n; i++) {
+                if (!words[i].equals(temp[i])) {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            System.out.println(flag);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public String findOrder(String[] dict, int N, int K) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < K; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            String s1 = dict[i];
+            String s2 = dict[i + 1];
+            int len = Math.min(s1.length(), s2.length());
+            boolean foundDifference = false;
+            for (int ptr = 0; ptr < len; ptr++) {
+                if (s1.charAt(ptr) != s2.charAt(ptr)) {
+                    adj.get(s1.charAt(ptr) - 'a').add(s2.charAt(ptr) - 'a');
+                    foundDifference = true;
+                    break;
+                }
+            }
+            
+            if (!foundDifference && s1.length() > s2.length()) {
+                return "";
+            }
+        }
+
+        List<Integer> topo = topoSort(K, adj);
+        
+        if (topo.size() != K) {
+            return "";
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int it : topo) {
+            ans.append((char)(it + 'a'));
+        }
+
+        return ans.toString();
+    }
+
+    private List<Integer> topoSort(int K, List<List<Integer>> adj) {
+        int[] inDegree = new int[K];
+        
+        for (int i = 0; i < K; i++) {
+            for (int node : adj.get(i)) {
+                inDegree[node]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < K; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            topo.add(node);
+
+            for (int neighbor : adj.get(node)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        return topo;
+    }
+}
