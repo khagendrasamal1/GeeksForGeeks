46979819f75cb11a1b3f2888afcfb9dcc4b778a3
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-08-26 12:06:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicate elements from sorted Array/remove-duplicate-elements-from-sorted-array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = sc.nextLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            Solution ob = new Solution();
+            int ans = ob.remove_duplicate(arr);
+            for (int i = 0; i < ans; i++) {
+                System.out.print(arr.get(i) + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to remove duplicates from the given array
+    public int remove_duplicate(List<Integer> arr) {
+        // Code Here
+        int i = 0;
+        for (int j = 1; j < arr.size(); j++) { // Fix: Iterate till arr.size()
+            if (!arr.get(i).equals(arr.get(j))) {
+                i++;
+                arr.set(i, arr.get(j));
+            }
+        }
+        return i + 1;
+    }
+}
