d682962b7b1f91f89b95fa7a7907c0e4a513e706
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-09-08 14:20:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
@@ -0,0 +1,124 @@
+//{ Driver Code Starts
+// driver
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+/* node for linked list
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static Node addTwoLists(Node num1, Node num2) {
+        // code here
+        // return head of sum list
+        num1 = reverse(num1);
+        num2 = reverse(num2);
+
+        Node dummyNode = new Node(0);
+        Node curr = dummyNode;
+        int carry = 0;
+
+        while (num1 != null || num2 != null || carry != 0) {
+            int sum = carry;
+            if (num1 != null) {
+                sum += num1.data;
+                num1 = num1.next;
+            }
+            if (num2 != null) {
+                sum += num2.data;
+                num2 = num2.next;
+            }
+            
+            carry = sum / 10;
+            curr.next = new Node(sum % 10);
+            curr = curr.next;
+        }
+
+        return reverse(dummyNode.next);
+    }
+}
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GfG {
+
+    static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(read.readLine());
+        while (T-- > 0) {
+
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            Node num1 = new Node(Integer.parseInt(str[0]));
+            Node tail = num1;
+            for (int i = 1; i < n; i++) {
+                int val = Integer.parseInt(str[i]);
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+
+            String str2[] = read.readLine().trim().split(" ");
+            int m = str2.length;
+
+            Node num2 = new Node(Integer.parseInt(str2[0]));
+            tail = num2;
+            for (int i = 1; i < m; i++) {
+
+                tail.next = new Node(Integer.parseInt(str2[i]));
+                tail = tail.next;
+            }
+
+            Solution g = new Solution();
+            Node res = g.addTwoLists(num1, num2);
+            printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
