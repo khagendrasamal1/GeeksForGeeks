a1252c4ace66a664353beedc9ee18404d2bdcace
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-09-29 20:12:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (long i : array) arr[idx++] = (int)i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.countZeroes(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int countZeroes(int[] arr) {
+        // code here
+        int count = 0;
+        
+        for(int num : arr){
+            if(num == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
