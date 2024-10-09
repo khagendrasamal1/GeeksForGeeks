f68fe8adeedebdffa41c88c52c01d0ac28ef4529
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-10-09 11:18:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Driver_code {
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+            return head;
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            Node head = null;
+
+            String str[] = read.readLine().trim().split(" ");
+            int listSize = str.length;
+            for (int i = 0; i < listSize; i++) {
+                head = insert(head, Integer.parseInt(str[i]));
+            }
+            boolean f = new Solution().isPalindrome(head);
+
+            System.out.println(f ? "true" : "false");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    // Function to check whether the list is palindrome.
+    Node reverseLL(Node head){
+        Node temp = head;
+        Node prev = null;
+        Node front;
+        
+        while(temp != null){
+            front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+    
+    boolean isPalindrome(Node head) {
+        // Your code here
+        
+        if(head == null || head.next == null){
+            return true;
+        }
+        
+        Node fast = head;
+        Node slow = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        Node newHead = reverseLL(slow);
+        
+        Node first = head;
+        Node second = newHead;
+        
+        while(second != null){
+            if(first.data != second.data){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
