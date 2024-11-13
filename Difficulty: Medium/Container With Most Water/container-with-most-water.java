6edf7b8ece7a03365a8eef1895f6af562cdbbee9
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-11-13 20:38:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().maxArea(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public int maxArea(int arr[]) {
+        // Code Here
+        int n = arr.length;
+        int i = 0;
+        int j = n - 1;
+        int maxArea = 0;  
+        
+        while (i < j) {
+            int area = Math.min(arr[i], arr[j]) * (j - i);
+            maxArea = Math.max(area, maxArea);
+
+            // Move the pointer with the shorter height
+            if (arr[i] < arr[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+}
