4ac89be3e4425ae7f102e7b0ebc65de525416a6c
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-12-01 15:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                String temp[] = br.readLine().trim().split(" ");
+                arr[i][0] = Integer.parseInt(temp[0]);
+                String x = temp[1];
+                arr[i][1] = Integer.parseInt(x);
+            }
+            Solution obj = new Solution();
+            // The mergeOverlap function now returns a List<int[]>
+            List<int[]> ans = obj.mergeOverlap(arr);
+
+            // Printing the merged arr
+            for (int[] interval : ans) {
+                System.out.print(interval[0] + " " + interval[1] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public List<int[]> mergeOverlap(int[][] arr) {
+        // Code here // Code here
+        
+        Arrays.sort(arr, Comparator.comparingInt(i -> i[0]));
+        
+        List<int[]> ans = new ArrayList<>();
+        
+        int newArr[] = arr[0];
+        ans.add(newArr);
+        
+        for(int[] a : arr){
+            if(a[0] <= newArr[1]){
+                newArr[1] = Math.max(a[1], newArr[1]);
+            }else{
+                newArr = a;
+                ans.add(newArr);
+            }
+        }
+        return ans;
+    }
+}
