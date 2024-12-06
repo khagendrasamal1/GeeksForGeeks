430dff24cef265a1dfab0e6ffeb2cfd6da6bea62
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-12-06 20:32:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Unique Paths/number-of-unique-paths.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking dimensions of the matrix
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    Solution ob = new Solution();
+		    //calling method NumberOfPath()
+		    System.out.println(ob.NumberOfPath(a,b));
+		
+System.out.println("~");
+}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find total number of unique paths.
+    public static int NumberOfPath(int a, int b) 
+    {
+        //Your code here
+        int dp[][] = new int[a][b];
+        
+        for(int i=0; i<a; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i=0; i<b; i++){
+            dp[0][i] = 1;
+        }
+        
+        for(int i=1; i<a; i++){
+            for(int j=1; j<b; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[a-1][b-1];
+    }
+}
