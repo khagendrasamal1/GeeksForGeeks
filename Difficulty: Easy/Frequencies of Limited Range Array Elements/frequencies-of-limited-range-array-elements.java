7c2b2d419b21cfc35997304be0e2279c6b6ddae2
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-12-19 20:25:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().frequencyCount(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to count the frequency of all elements from 1 to N in the array.
+    public List<Integer> frequencyCount(int[] arr) {
+        // do modify in the given array
+        int n = arr.length;
+        
+        Map<Integer, Integer> m = new HashMap<>();
+        
+        for(int num : arr){
+            m.put(num, m.getOrDefault(num, 0)+1);
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++){
+            ans.add(m.getOrDefault(i, 0));
+        }
+        return ans;
+    }
+}
