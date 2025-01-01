750c19c9000c4fd5494c49404280b3fcc33168cf
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-01-02 01:14:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (long i : array) arr[idx++] = (int)i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.firstRepeated(arr));
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the position of the first repeating element.
+    public static int firstRepeated(int[] arr) {
+        // Your code here
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        
+        for(int i=0; i<arr.length; i++){
+            if(map.get(arr[i]) > 1){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
