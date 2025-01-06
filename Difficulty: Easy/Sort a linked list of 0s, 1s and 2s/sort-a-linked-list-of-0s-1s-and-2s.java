a29366f63508528cba545baf0dd38a86c22d3a91
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-01-06 20:08:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution {
+    // Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head) {
+        // add your code here
+        Node zeroHead = new Node(0), oneHead = new Node(0), twoHead = new Node(0);
+        Node zeroTail = zeroHead, oneTail = oneHead, twoTail = twoHead;
+
+        Node curr = head;
+        while (curr != null) {
+            if (curr.data == 0) {
+                zeroTail.next = curr;
+                zeroTail = zeroTail.next;
+            } else if (curr.data == 1) {
+                oneTail.next = curr;
+                oneTail = oneTail.next;
+            } else {
+                twoTail.next = curr;
+                twoTail = twoTail.next;
+            }
+            curr = curr.next;
+        }
+
+        zeroTail.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
+        oneTail.next = twoHead.next;
+        twoTail.next = null;
+
+        return zeroHead.next;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); i++) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+            head = new Solution().segregate(head);
+            printList(head);
+        }
+    }
+}
+// } Driver Code Ends
