6260fc02479226b03ff2ae75b319d444c6e96ae8
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-01-15 12:15:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
+
+        while (t-- > 0) {
+            String line = read.readLine().trim(); // Read the array input
+            String[] numsStr = line.split(" ");   // Split the input string by spaces
+            int[] nums =
+                new int[numsStr.length]; // Convert string array to integer array
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int k = Integer.parseInt(read.readLine().trim()); // Read target sum
+
+            Solution ob = new Solution();
+            int ans = ob.longestSubarray(nums, k); // Call the function and store result
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        
+        int sum = 0;
+        int len = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            
+            if(sum == k)    len = i+1;
+            
+            if(map.containsKey(sum - k)){
+                len = Math.max(len, i - map.get(sum - k));
+            }
+            
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
+        }
+        return len;
+    }
+}
