3b8bee606cc25fe6e51bf8f323e77c1987922851
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-03-17 10:48:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number Of Open Doors/number-of-open-doors.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long n = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.noOfOpenDoors(n));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int noOfOpenDoors(Long n) {
+        // code here
+        return (int) Math.sqrt(n);
+    }
+};
