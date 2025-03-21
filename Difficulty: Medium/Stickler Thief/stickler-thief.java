b96c0e2207ea51850c0b83303fb6529a785fa47c
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-03-21 08:21:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.java
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -1,53 +1,44 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class GFG
- {
-	public static void main (String[] args)
-	 {
-	  
-	  //taking input using Scanner class
-	  Scanner sc = new Scanner(System.in);
-	  
-	  //taking count of testcases
-	  int t = sc.nextInt();
-	  while(t-- > 0){
-	      
-	      //taking count of houses
-	      int n = sc.nextInt();
-	      int arr[] = new int[n];
-	      
-	      //inserting money present in 
-	      //each house in the array
-	      for(int i = 0; i<n; ++i)
-	           arr[i] = sc.nextInt();
-  	      
-  	      //calling method FindMaxSum() of class solve
-  	      System.out.println(new Solution().FindMaxSum(arr, n));
-	 }
-   }
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            // calling method findMaxSum() of class solve
+            System.out.println(new Solution().findMaxSum(arr));
+            System.out.println("~");
+        }
+    }
 }
 // } Driver Code Ends
 
 
-class Solution
-{
-    //Function to find the maximum money the thief can get.
-    public int FindMaxSum(int arr[], int n)
-    {
-        // Your code here
-        if(n==0)    return 0;
-        if(n==1)    return arr[0];
+class Solution {
+    public int findMaxSum(int arr[]) {
+        // code here
+        int n = arr.length;
         
-        int a[] = new int[n];
+        if(n < 2)   return arr[0];
         
-        a[0] = arr[0];
-        a[1] = Math.max(arr[0], arr[1]);
+        int dp[] = new int[n];
+        
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
         
         for(int i=2; i<n; i++){
-            a[i] = Math.max((a[i-2]+arr[i]), a[i-1]);
+            dp[i] = Math.max(dp[i-2]+arr[i], dp[i-1]);
         }
-        return a[n-1];
+        return dp[n-1];
     }
 }
