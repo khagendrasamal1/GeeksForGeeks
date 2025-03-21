eedc7d7d67c3d93ba21624782f1a3f7b77e077b4
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-03-21 17:57:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input (arr)
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] =
+                    Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input (dep)
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] dep = new int[m];
+            for (int i = 0; i < m; i++) {
+                dep[i] =
+                    Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.findPlatform(arr, dep));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[]) {
+        // add your code here
+        int n = arr.length;
+        
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int cnt = 0, max = 0;
+        
+        int i =0, j = 0;
+        
+        while(i < n){
+            if(arr[i] <= dep[j]){
+                cnt++;
+                max = Math.max(max, cnt);
+                i++;
+            }else if(arr[i] > dep[j]){
+                cnt--;
+                j++;
+            }
+        }
+        return max;
+    }
+}
