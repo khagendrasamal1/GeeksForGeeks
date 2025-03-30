2389433fae377091bd90a1ba0f86bac3a2123271
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-03-30 09:57:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            line = read.readLine();
+            tokens = line.split(" ");
+            for (String token : tokens) {
+                array2.add(Integer.parseInt(token));
+            }
+
+            // ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] gas = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) gas[idx++] = i;
+
+            int[] cost = new int[array2.size()];
+            idx = 0;
+            for (int i : array2) cost[idx++] = i;
+
+            int ans = new Solution().startStation(gas, cost);
+
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        // Your code here
+        int n = gas.length;
+        
+        int sum = 0, total = 0, position = 0;
+        
+        for(int i=0; i<n; i++){
+            sum += gas[i] - cost[i];
+            
+            if(sum < 0){
+                total += sum;
+                sum = 0;
+                position = i+1;
+            }
+        }
+        total += sum;
+        
+        return (total >= 0) ? position : -1;
+    }
+}
