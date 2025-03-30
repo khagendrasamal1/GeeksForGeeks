158038451c18f6fb6520e90992022d612f19ef11
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-03-30 15:43:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t =
+            Integer.parseInt(scanner.nextLine().trim()); // Read number of test cases
+
+        while (t-- > 0) {
+            String line = scanner.nextLine().trim(); // Read the entire line
+            String[] numsStr = line.split(" "); // Split the line into string numbers
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] =
+                    Integer.parseInt(numsStr[i]); // Convert each string to integer
+            }
+
+            int ans = new Solution().arraySum(
+                nums); // Compute the sum using the Solution class
+
+            System.out.println(ans); // Print the result
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int arraySum(int arr[]) {
+        // code here
+        int sum = 0;
+        
+        for(int num : arr){
+            sum += num;
+        }
+        return sum;
+    }
+}
