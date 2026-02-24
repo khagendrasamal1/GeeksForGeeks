761647f7c803a0eaa40c1ed46d595e6bccde4f81
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2026-02-24 22:41:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+	def arraySum(self, arr):
+   		# code here
+   		total = 0
+   		
+   		for num in arr:
+   		    total += num
+   		    
+   		return total
