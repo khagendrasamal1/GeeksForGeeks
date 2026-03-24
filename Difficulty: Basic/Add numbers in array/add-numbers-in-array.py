2964caa85432f76972ab05db0c1e1d04bc6883c9
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2026-03-24 14:32:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Add numbers in array/add-numbers-in-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def get_Sum(self, n, input):
+        # code here
+        ans = 0
+        
+        for num in input:
+            ans += num
+            
+        return ans
+        
