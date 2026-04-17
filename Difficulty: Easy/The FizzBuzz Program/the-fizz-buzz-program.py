d22b6f00820919c03270a907f3631efbb453fdbc
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2026-04-17 12:28:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fizzBuzz(self, number):
+        # code here.
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
